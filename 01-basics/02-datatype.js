c739135eb395cb3af69fc894ec36ceaa5498b75a
Muhammad Saqib <captsaqibsaqib@gmail.com>
2024-10-28 08:38:55 +0000

    datatype

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) // we are using node js not browser
+
+console.log(3 + 3)
+
+console.log("saqib")
+
+
+let name = "saqib"
+let age = 24
+let isLoggedIn = false
+
+
+//numbar=>2 to power 53
+//bignit
+//booleean=> true/false
+//null => standloan value
+// symbol => unique
+
+//object
+
+console.log(typeof"undefined")
+
